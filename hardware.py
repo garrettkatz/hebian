fb0262ea15bc0378b34fe29999924f91b969b0c4
Garrett Katz <gkatz01@syr.edu>
2025-10-15 13:48:36 -0400

    added base movement to hardware api

--- hardware.py
+++ hardware.py
@@ -22,8 +22,9 @@ class RosieWrapper:
         lu = hebi.Lookup()
         self.arm_group = lu.get_group_from_names(['Rosie'], ['J1_base','J2_shoulder','J3_elbow','J4_wrist1','J5_wrist2','J6_wrist3'])
         self.grip_group = lu.get_group_from_names(['Rosie'], ['gripperSpool'])
+        self.base_group = lu.get_group_from_names(['Rosie'], ['W1','W2','W3'])
 
-        for group in (self.arm_group, self.grip_group):
+        for group in (self.arm_group, self.grip_group, self.base_group):
             if group is not None:
                 group.command_lifetime = command_lifetime
 
@@ -36,10 +37,11 @@ class RosieWrapper:
         current = gf.position
         return current
     
-    def goto_position(self, position, duration = 2.):
+    def goto_position(self, position, duration = 2., command_frequency=100):
         """
         position: numpy array of goal joint angles
         duration: in seconds
+        command_frequency: in Hz
         """
         if self.arm_group is None:
             print("None group!")
@@ -55,7 +57,7 @@ class RosieWrapper:
         current = gf.position
     
         # interpolate slow trajectory
-        num_pts = 200
+        num_pts = int(duration * command_frequency)
         interp = np.linspace(0, 1, num_pts)[:,np.newaxis]
         traj = interp * position + (1-interp) * current
     
@@ -64,7 +66,79 @@ class RosieWrapper:
         for t, pos in enumerate(traj):
             gc.position = pos
             self.arm_group.send_command(gc)
-            time.sleep(duration / num_pts)
+            time.sleep(1. / command_frequency)
+
+    def rotate_base(self, angle, duration = 2., command_frequency=100):
+        """
+        angle: angular change in base heading (float)
+            positive rotates counter-clockwise when viewed from above
+        duration: in seconds
+        command_frequency: in Hz
+        """
+
+        # get current position
+        gf = hebi.GroupFeedback(3)
+        gf = self.base_group.get_next_feedback(reuse_fbk=gf)
+        current = gf.position
+
+        # subtract angle to match counter-clockwise convention
+        position = current - angle
+
+        # interpolate slow trajectory
+        num_pts = int(duration * command_frequency)
+        interp = np.linspace(0, 1, num_pts)[:,np.newaxis]
+        traj = interp * position + (1-interp) * current
+    
+        # stream the commands
+        gc = hebi.GroupCommand(3)
+        for t, pos in enumerate(traj):
+            gc.position = pos
+            self.base_group.send_command(gc)
+            time.sleep(1. / command_frequency)
+
+
+    def move_base(self, xy_delta, duration = 2., command_frequency=100):
+        """
+        xy_delta: numpy array of target changes in base xy coordinates
+            positive x-axis points "forward" (side where power buttons are)
+            positive y-axis points to the left when facing "forward"
+        duration: in seconds
+        command_frequency: in Hz
+        """
+        if self.base_group is None:
+            print("None group!")
+            return
+
+        if 2 != len(xy_delta):
+            print("size mismatch")
+            return
+
+        # convert xy delta to wheel delta
+        # based on https://github.com/HebiRobotics/hebi-python-examples/blob/main/kits/bases/omni_base.py
+        wheel_delta =  np.array(
+            [[-11.36516278,  -6.56167979],
+             [ 11.36516278,  -6.56167979],
+             [  0.        ,  13.12335958]]) @ xy_delta
+
+        # get current position
+        gf = hebi.GroupFeedback(3)
+        gf = self.base_group.get_next_feedback(reuse_fbk=gf)
+        current = gf.position
+
+        # convert wheel delta to target position
+        position = current + wheel_delta
+
+        # interpolate slow trajectory
+        num_pts = int(duration * command_frequency)
+        interp = np.linspace(0, 1, num_pts)[:,np.newaxis]
+        traj = interp * position + (1-interp) * current
+    
+        # stream the commands
+        gc = hebi.GroupCommand(3)
+        for t, pos in enumerate(traj):
+            gc.position = pos
+            self.base_group.send_command(gc)
+            time.sleep(1. / command_frequency)
 
     def open(self):
         if self.grip_group is None:
